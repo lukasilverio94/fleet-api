3a27fd4b2c857fb8bcbeaf61871795bf6c3d2e8c
Lucas Silverio <lukasilverio94@gmail.com>
2026-06-08 22:37:54 -0300

    init project: setup postgres docker

---
+++ src/main/java/br/com/lucasdev/fleetapi/FleetApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasdev.fleetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasdev/fleetapi/FleetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasdev.fleetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
